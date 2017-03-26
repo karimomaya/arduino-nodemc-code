6e264518d4cccb24eca954c0444bd7b776f7292b
Ahmed Abdelhameed <ahmed_fcis@hotmail.com>
2017-03-27 00:01:03 +0200

    update RServer

--- lib/OpenRobotServer/RServer.cpp
+++ lib/OpenRobotServer/RServer.cpp
@@ -1,23 +1,27 @@
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h> 
-#include <ESP8266WebServer.h>
-#include <ESP8266HTTPUpdateServer.h>
-#include <FS.h>
 #include <RServer.h>
 
 RServer* RServer::pThis = NULL;
 RServer::RServer(int port):webServer(port){
-	 updateServer.setup(&webServer);
-	 pThis = this;
-	 
-	 RConfig config;
-	 loadConfig(config);
-	 
-	 bool bconnected=false;
-	 if(config.flags & RCONFIG_ENABLE_STA)
-		 bconnected = connectSTA(config);
-	 if(config.flags & RCONFIG_ENABLE_AP || (!bconnected && (config.flags & RCONFIG_ENABLE_AP_IF_NO_STA)))
-		 configureAP(config);
+
+	delay(1000);// is this required?
+	pThis = this;
+	
+	updateServer.setup(&webServer);
+}
+
+void RServer::setupServer() {
+	openDirectory();
+	
+	loadConfig(config);
+	
+	bool bconnected=false;
+	if(config.flags & RCONFIG_ENABLE_STA)
+		bconnected = connectSTA(config);
+	if(config.flags & RCONFIG_ENABLE_AP || (!bconnected && (config.flags & RCONFIG_ENABLE_AP_IF_NO_STA)))
+		configureAP(config);
+
+	registerRequestHandlers();
+	begin();
 }
 
 void RServer::begin(){
@@ -37,21 +41,108 @@ void RServer::stop(){
 }
 
 //try to load config from config file if can't load default config
-void RServer::loadConfig(RConfig& config){
+bool RServer::loadConfig(RConfig& config){
 	
-	
+	if(SPIFFS.exists("/config.txt")){
+		File file = SPIFFS.open("/config.txt", "r");
+		file.read((uint8_t*)&config, sizeof(RConfig));
+		file.close();
+		return true;
+	}
+	return false;
 }
 
 //save config to config file
 void RServer::saveConfig(RConfig& config){
-	
+	File file = SPIFFS.open("/config.txt", "w");
+	file.write((uint8_t*)&config, sizeof(RConfig));
+	file.close();
 }
 
-//handle '/' request and sent index.html
-void RServer::handleRoot(){
-	if(!pThis->checkPermission())
-		return;
+// to open data folder
+void RServer::openDirectory() {
+	SPIFFS.begin();
+    Dir dir = SPIFFS.openDir("/");
+}
+
+
+void RServer::registerRequestHandlers(){
+
+	webServer.on("/", pThis->handleRoot);
 	
+    webServer.on("/submit", pThis->handleWifiSubmittion);
+	
+    webServer.on("/list_wifi", HTTP_GET, [](){
+		  if(!(pThis->checkPermission()))
+			return;
+
+		  String json = pThis->listAvailableWIFI();
+		  pThis->webServer.send(200, "text/json", json);
+		  json = String();
+    });
+}
+
+String RServer::listAvailableWIFI() {
+	logging("** Scan Networks **");
+	
+    int numSsid = WiFi.scanNetworks();
+    if (numSsid == -1) {
+    	logging("Couldn't get a wifi connection");
+        return "[]";
+    }
+	
+    String json = "[";  
+    for (int thisNet = 0; thisNet < numSsid; thisNet++) {
+    
+      logging(WiFi.SSID(thisNet).c_str());
+      
+      if (thisNet != 0)
+		json += "},";
+      
+	  json += "{\"name\":\""+WiFi.SSID(thisNet)+"\"";
+    }
+
+    json += "}]";
+	
+    return json;
+}
+
+
+void RServer::handleRoot() {
+	if(!(pThis->checkPermission()))
+			return;
+		
+	File file = SPIFFS.open("/index.html", "r");
+	size_t sent = pThis->webServer.streamFile(file, "text/html");
+	file.close();//??
+}
+
+void RServer::handleWifiSubmittion() {
+	if(!(pThis->checkPermission()))
+			return;
+		
+	if (pThis->webServer.args() < 2 ) 
+		return;
+    
+    for ( uint8_t i = 0; i < pThis->webServer.args(); i++ ) {
+      if (pThis->webServer.argName(i) == "wifiname") {
+		strcpy(pThis->config.STAName, pThis->webServer.arg(i).c_str());
+      }
+      else if (pThis->webServer.argName(i) == "password") {
+      	strcpy(pThis->config.STAPassword, pThis->webServer.arg(i).c_str());
+      }
+    }
+
+    pThis->stopAP();
+    pThis->connectSTA(pThis->config);
+    pThis->registerRequestHandlers();
+ 
+}
+
+void RServer::stopAP() {
+	pThis->stop();
+	WiFi.disconnect();
+    WiFi.mode(WIFI_OFF);
 }
 
 //update config and save config file
@@ -70,18 +161,26 @@ void RServer::handleGetConfig(){
 
 
 IPAddress RServer::configureAP(RConfig& config){
-	
+
+	WiFi.softAP(pThis->config.STAName, pThis->config.STAPassword);
+  	return WiFi.softAPIP();
 }
 
 //if connected to wifi return true
 bool RServer::connectSTA(RConfig& config){
-	
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(pThis->config.STAName, pThis->config.STAPassword);
+   	while (WiFi.status() != WL_CONNECTED) {
+    	delay(500);
+    	Serial.print(".");
+   	}
+   	Serial.println(WiFi.localIP());
 	return true;
 }
 
-bool checkPermission(){
+bool RServer::checkPermission(){
 	return true;
 }
-void RServer::logging(String& msg){
-	
+void RServer::logging(const char* msg){
+	Serial.println(msg);
 }
--- lib/OpenRobotServer/RServer.h
+++ lib/OpenRobotServer/RServer.h
@@ -1,8 +1,11 @@
-
 #ifndef _OPEN_ROBOT_H_
 
 #define _OPEN_ROBOT_H_
-
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPUpdateServer.h>
+#include <FS.h>
 #define RCONFIG_VER						0x01
 #define RCONFIG_ENABLE_AP				0x01
 #define RCONFIG_FIXED_AP_IP				0x02
@@ -10,8 +13,8 @@
 #define RCONFIG_ENABLE_STA				0x08
 #define RCONFIG_FIXED_STA_IP			0x10
 
-#define RECONFIG_DEF_AP_NAME		"kareem-arduino"
-#define RECONFIG_DEF_AP_PASSWORD	"123456789"	
+#define RECONFIG_DEF_AP_NAME		 ((char *)"kareem-arduino")
+#define RECONFIG_DEF_AP_PASSWORD	 ((char *)"123456789")	
 
 struct RConfig{
 	int  ver;
@@ -28,32 +31,40 @@ class RServer{
 	
 public: 
 	RServer(int port);
-
+	
 	void begin();
 	void handleClient();
 
+	void setupServer();
+
 	void close();
 	void stop();
-  
+  	void openDirectory();
 private:
 
 	ESP8266WebServer 		webServer;
 	ESP8266HTTPUpdateServer	updateServer;
+	RConfig 				config;
 	
-	void loadConfig(RConfig& config);
+	bool loadConfig(RConfig& config);
 	void saveConfig(RConfig& config);
 	
+	void registerRequestHandlers();
+
 	static RServer* pThis;
 	static void handleRoot();
+	static void handleWifiSubmittion();
 	static void handleSaveConfig();
 	static void handleGetConfig();
+	static void stopAP();
 	
+	String listAvailableWIFI();
 	bool checkPermission();
 	
 	IPAddress configureAP(RConfig& config);
 	bool connectSTA(RConfig& config);
 	
-	void logging(String& msg);
+	void logging(const char* msg);
 };
 
 #endif 
